a70110029d11e818d364b673d1b13ff4088603e6
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-03 22:12:06 +0530

    Add files via upload

---
+++ CWH Playlist Java/video_13.java
@@ -0,0 +1,9 @@
+// String a sequence of characters
+public class video_13{
+    public static void main(String[] args){
+        // String name = new String("Rajat");
+        String name = "Rajat Bhardwaj";
+        System.out.print("The name is: ");
+        System.out.print(name);
+    }
+}

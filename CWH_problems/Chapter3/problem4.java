9ae5e266fdc602aff011a5f6019ef29bb03b33a6
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-10 18:18:39 +0530

    Add files via upload

---
+++ CWH_problems/Chapter3/problem4.java
@@ -0,0 +1,9 @@
+public class problem4{
+    public static void main(String[] args){
+        String spc = "Hello Good Morning  How are   you";
+
+        System.out.println("Index: "+spc.indexOf("  "));
+        System.out.println("INdex: "+spc.indexOf("   "));
+
+    }
+}

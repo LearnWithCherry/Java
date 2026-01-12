8457188440bf4b7f05972191b04d0f1653e96d55
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-12 22:37:07 +0530

    Add files via upload

---
+++ CWH_problems/Chapter5/p03.java
@@ -0,0 +1,14 @@
+public class p03{
+    public static void main(String [] args){
+        // Normal 
+        // int table = 2;
+        // for (int i=1; i<=10; i++){
+            // System.out.println("table of 2 = " + i * table);
+        // }
+        // Reverse 
+        int table = 2;
+        for (int i=10; i>=1; i--){
+            System.out.println("table of 2 = " + i * table);
+        }
+    }
+}

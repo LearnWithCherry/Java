6fe78652cdf0b798b32123dc571f95c2771443e5
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-12 22:37:59 +0530

    Add files via upload

---
+++ CWH_problems/Chapter5/p07.java
@@ -0,0 +1,10 @@
+public class p07{
+    public static void main(String [] args){
+        int table = 8;
+        int count = 0;
+        for(int i=1; i<=10; i++){
+            count += i * table;
+        }
+        System.out.print(count);
+    }   
+}

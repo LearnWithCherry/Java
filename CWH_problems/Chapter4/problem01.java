cba67b20df6e37b8a7cdbc2d9f575cc7b10c9740
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-12 21:26:23 +0530

    Add files via upload

---
+++ CWH_problems/Chapter4/problem01.java
@@ -0,0 +1,10 @@
+public class problem01{
+    public static void main(String [] args){
+        int a = 10;
+        if (a == 11){
+            System.out.println("I am 11");
+        }else{
+            System.out.println("I am 10");
+        }
+    }
+}

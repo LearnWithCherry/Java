39eccdc79da723607947253b2cdf299cd6c2d465
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-12 22:37:35 +0530

    Add files via upload

---
+++ CWH_problems/Chapter5/p05.java
@@ -0,0 +1,13 @@
+public class p05{
+    public static void main(String [] args){
+        int i =0;
+        while(true){
+            if (i == 5){
+                break;
+            }
+            System.out.println("5");
+            i++;
+        }
+        
+    }
+}

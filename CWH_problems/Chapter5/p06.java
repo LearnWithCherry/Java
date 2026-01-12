b328f46b596d78d8f42fc261a72ed3dc921da942
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-12 22:37:47 +0530

    Add files via upload

---
+++ CWH_problems/Chapter5/p06.java
@@ -0,0 +1,20 @@
+public class p06{
+    public static void main(String [] args){
+        // using while loop
+        int i =0;
+        System.out.println("Print using While loop");
+        while(true){
+            if (i == 5){
+                break;
+            }
+            System.out.println("1");
+            i++;
+        }
+        // using for loop 
+            System.out.println("Print using for loop");
+
+        for (int j=0; j<5; j++){
+            System.out.println("1");
+        }
+    }
+}

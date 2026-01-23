4e70a99a300f69a35fe09a39edb5e97a1812fd62
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-01-23 23:14:27 +0530

    Add files via upload

---
+++ CSE 310 LPU/main.java
@@ -0,0 +1,22 @@
+// public class main{ 
+//     public static void main(String[] args){
+//         System.out.print("Hello World");
+//     }
+// }
+
+// WAP that prints ur name using command line 
+
+public class main{ 
+    public static void main(String[] args){
+        System.out.print("Hello World");
+        System.out.println(args[0]);
+    }
+}
+
+// how to run:
+    // javac main.java
+    // java main 324QP
+    // In terminal 
+    // [0] is index 
+
+

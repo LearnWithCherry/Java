80aef33a201155423832e69fc95290b538c3aedd
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-04-23 01:17:53 +0530

    Add files via upload

---
+++ Problems/first.java
@@ -0,0 +1,12 @@
+public class first{
+    public static void main(String[]args){
+        System.out.print("Hello...World");
+        int x = 0;
+        while(x++ < 5) {
+            System.out.print(x);
+        x++;
+    }
+    System.out.println(x);
+
+    }
+}

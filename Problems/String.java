491cbdc25a861e219c0ca78adfb4de42c2115cfc
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-05-09 12:23:47 +0530

    Add main class for review details input

---
+++ Problems/String.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+// You are using Java
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // int x = sc.nextInt();
+        String y = sc.nextLine();
+        // System.out.println("Recent Review Rating: "+x+" stars");
+        System.out.println("Review Details: " + y);
+    }
+}

78a4bf6ba241275c280a20641dc6094d6f1e4ad1
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-05-15 17:05:16 +0530

    Create Main.java

---
+++ CSE 310 LPU/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40};
+        int val = nums[nums.length - 1] / nums[1];
+        System.out.println(val);
+    }
+}

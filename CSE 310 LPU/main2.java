5b9e3110d090f3a708712124a4e42134052cbb3b
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-05-15 17:10:11 +0530

    Create main2.java

---
+++ CSE 310 LPU/main2.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = { {1, 2}, {3, 4, 5} };
+        System.out.println(matrix[1].length + matrix[0][1]);
+    }
+}

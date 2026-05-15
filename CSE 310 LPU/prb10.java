168dcd4b2420543cfef8f49ac58075cdd658534a
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-05-15 17:45:43 +0530

    Add prb10.java with nested loop example

---
+++ CSE 310 LPU/prb10.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (i == j) continue;
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}

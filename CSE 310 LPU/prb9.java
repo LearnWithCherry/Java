c732fa035bb44a29d3b741814ddf157e4867cf2f
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-05-15 17:45:28 +0530

    Add Main class with a while loop example

---
+++ CSE 310 LPU/prb9.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        while (x > 0) {
+            x--;
+            if (x == 2) break;
+        }
+        System.out.println(x);
+    }
+}

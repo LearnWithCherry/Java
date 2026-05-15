4f3f1f8d6caa7811819b3f0adcba6f63b1589849
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-05-15 13:59:42 +0530

    Add files via upload

--- Problems/Main.java
+++ Problems/Main.java
@@ -1,59 +1,9 @@
-
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        A ob1 = new A();
-        ob1.start();
-        B ob2 = new B();
-        ob2.start();
-        for(int i=0; i<25; i++){
-            System.out.println("Task Done");
-        }
-    }
-} 
-class A extends Thread{
-    public void run(){
-        for(int i=0; i<25; i++){
-            System.out.println("A"+i);
-        }
-    }
-} 
-class B extends Thread{
-    public void run(){
-        for(int i=0; i<25; i++){
-            System.out.println("B"+i);
-        }
-    }
-}
-
-class Main {
-    public static void main(String[] args) throws InterruptedException {
-        Counter c = new Counter();
-        Thread t1 = new Thread(() -> {
-            for (int i = 0; i < 2000; ++i) {
-                c.increment();
-            }
-        });
-        Thread t2 = new Thread(() -> {
-            for (int i = 0; i < 2000; ++i) {
-                c.increment();
-            }
-        });
-        t1.start();
-        t2.start();
-        t1.join();
-        t2.join();
-        System.out.println("Count: " + c.count());
-    }
-}
-
-class Counter {
-    int count;
-
-    synchronized void increment() {
-        count++;
-    }
-
-    synchronized int count() {
-        return count;
+        // byte b = 127;
+        // b++;
+        // System.out.println(b);
+        System.out.println(10 % -3);
+        System.out.println(10 >> 1);
     }
 }

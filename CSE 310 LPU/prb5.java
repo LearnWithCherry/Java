3a1e7bbe76052bc101369fba6c93535911808809
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-05-15 17:44:21 +0530

    Create prb5.java

---
+++ CSE 310 LPU/prb5.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "LPU";
+        String s2 = s1.replace('L', 'M');
+        System.out.println(s1 + " " + s2);
+    }
+}

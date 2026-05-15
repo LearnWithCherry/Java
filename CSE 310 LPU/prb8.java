11b488871fb285ddf1091ca400d3e79244e6173d
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-05-15 17:45:09 +0530

    Create prb8.java

---
+++ CSE 310 LPU/prb8.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Java");
+        sb.append("11");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}

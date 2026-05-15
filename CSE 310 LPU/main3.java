752a3edb17bd7c2591a134d5b1d8832da535a3c1
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-05-15 17:17:28 +0530

    Add main3.java to compare string equality

---
+++ CSE 310 LPU/main3.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "HELLO";
+        System.out.println(s1.equalsIgnoreCase(s2) + " " + s1.equals(s2));
+    }
+}
